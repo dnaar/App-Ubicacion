9b62dbe50f8fa5340026bddc2b1d03c370aea44f
David Naar <dnaar@uninorte.edu.co>
2020-08-24 21:20:08 -0500

    Arreglado bug de conexión
    Añadido TimeOut de conexión para arreglar errores.

--- TioRico/app/src/main/java/com/richuncle/tioricov4/TCP_Sender.java
+++ TioRico/app/src/main/java/com/richuncle/tioricov4/TCP_Sender.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class TCP_Sender extends AsyncTask<String, Void, Void> {
@@ -15,7 +16,9 @@ public class TCP_Sender extends AsyncTask<String, Void, Void> {
     protected Void doInBackground(String... voids) {
         String message = voids[0];
         try{
-            server = new Socket("192.168.0.15",8080);
+            server = new Socket();
+            server.connect(new InetSocketAddress("192.168.0.15", 8080), 2000);
+            server.setSoTimeout(2000);
             pw = new PrintWriter(server.getOutputStream());
             pw.write(message);
             pw.flush();
